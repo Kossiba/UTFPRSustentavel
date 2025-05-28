2d70628886f15a8e79b777485369f6e6b0527826
Kossiba <gabrielkosibamelo@alunos.utfpr.edu.br>
2025-05-28 12:31:04 -0300

    iniciando projeto

---
+++ src/test/java/utfpr/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package utfpr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
