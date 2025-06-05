41823773d85340464033ff7e2eae28f98fb41042
Kossiba <150734694+Kossiba@users.noreply.github.com>
2025-06-05 18:03:34 -0300

    Update WebConfig.java

--- src/main/java/utfpr/backend/config/WebConfig.java
+++ src/main/java/utfpr/backend/config/WebConfig.java
@@ -1,5 +1,4 @@
-package utfpr.backend.config; // ajuste para o seu pacote
-
+package utfpr.backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
